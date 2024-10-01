fc396f8be2662a79acb3446105645722010fd089
KirillBurdov <91791699+KirillBurdovS@users.noreply.github.com>
2024-10-01 17:43:37 +0300

    Task7
    Task7

---
+++ Tyuiu.BurdovKS.Sprint1.Task7.V30.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+
+
+namespace Tyuiu.BurdovKS.Sprint1.Task7.V30.Lib
+{
+    public class DataService : ISprint1Task7V30
+    {
+        public double Calculate(double x, double y)
+        {
+
+
+            double z = x + Math.Exp(x) + (Math.Pow(x, 3) + Math.Sin(Math.Pow(x, 5))) / Math.Pow(3, x) + Math.Pow(y, 5) / Math.Pow(5, y);
+
+            double zR = Math.Round(z, 3);
+
+
+            return zR;
+
+
+
+
+
+        }
+    }
+}
