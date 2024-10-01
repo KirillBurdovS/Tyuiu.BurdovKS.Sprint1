38d5000775135bf87d2a487d2b590d1caca7a287
KirillBurdov <91791699+KirillBurdovS@users.noreply.github.com>
2024-10-01 15:07:00 +0300

    Task6
    Task6

---
+++ Tyuiu.BurdovKS.Sprint1.Task6.V16.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+
+namespace Tyuiu.BurdovKS.Sprint1.Task6.V16.Lib
+{
+    public class DataService : ISprint1Task6V16
+    {
+        public bool CheckSpecSymbols(string value)
+        {
+
+            return value.Contains('!') && value.Contains('?');
+
+
+
+        }
+    }
+}
